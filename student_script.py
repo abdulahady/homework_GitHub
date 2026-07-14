8bd1ca0e2f94ecfd6aac6e12fbc51c3505dd73f4
Mohammed Ashvaq <mashvaq@lxplus9108.cern.ch>
2026-07-14 19:03:54 +0200

    Version 2: change bins to 100 and entries to 5000

--- student_script.py
+++ student_script.py
@@ -5,9 +5,9 @@ PyROOT homework: create, fill, and print histogram entries
 """
 import ROOT
 # Create a 1D histogram: 50 bins from -5 to 5
-h = ROOT.TH1F("h", "Random Uniform;x;Entries", 50, -5, 5)
+h = ROOT.TH1F("h", "Random Uniform;x;Entries", 100, -5, 5)
 # Fill with 1000 uniform random numbers
-for i in range(1000):
+for i in range(5000):
     h.Fill(ROOT.gRandom.Uniform(-5, 5))
 # Print number of entries
 print("Histogram name:", h.GetName())
